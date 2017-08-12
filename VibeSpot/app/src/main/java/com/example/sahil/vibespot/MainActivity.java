3181e6839ff3208d7a81284749aeb2fb7a005c32
sahilmazmudar <sahil.maz97@gmail.com>
2017-08-11 20:23:44 -0700

    MainActivity updated, need to debug

--- VibeSpot/app/src/main/java/com/example/sahil/vibespot/MainActivity.java
+++ VibeSpot/app/src/main/java/com/example/sahil/vibespot/MainActivity.java
@@ -1,13 +1,91 @@
 package com.example.sahil.vibespot;
 
-import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
+        import android.app.Activity;
+        import android.content.Intent;
+        import android.os.Bundle;
+        import android.util.Log;
 
-public class MainActivity extends AppCompatActivity {
+        import com.spotify.sdk.android.authentication.AuthenticationClient;
+        import com.spotify.sdk.android.authentication.AuthenticationRequest;
+        import com.spotify.sdk.android.authentication.AuthenticationResponse;
+        import com.spotify.sdk.android.player.Config;
+        import com.spotify.sdk.android.player.ConnectionStateCallback;
+        import com.spotify.sdk.android.player.Error;
+        import com.spotify.sdk.android.player.Player;
+        import com.spotify.sdk.android.player.PlayerEvent;
+        import com.spotify.sdk.android.player.Spotify;
+        import com.spotify.sdk.android.player.SpotifyPlayer;
+
+public class MainActivity extends Activity implements
+        SpotifyPlayer.NotificationCallback, ConnectionStateCallback
+{
+
+    // TODO: Replace with your client ID
+    private static final String CLIENT_ID = "6c54b390f0634df58a3833586b1dd235";
+    // TODO: Replace with your redirect URI
+    private static final String REDIRECT_URI = "custom-vibeu-protocol://callback";
+
+    private Player mPlayer;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent intent) {
+        super.onActivityResult(requestCode, resultCode, intent);
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+    }
+
+    @Override
+    public void onPlaybackEvent(PlayerEvent playerEvent) {
+        Log.d("MainActivity", "Playback event received: " + playerEvent.name());
+        switch (playerEvent) {
+            // Handle event type as necessary
+            default:
+                break;
+        }
+    }
+
+    @Override
+    public void onPlaybackError(Error error) {
+        Log.d("MainActivity", "Playback error received: " + error.name());
+        switch (error) {
+            // Handle error type as necessary
+            default:
+                break;
+        }
+    }
+
+    @Override
+    public void onLoggedIn() {
+        Log.d("MainActivity", "User logged in");
+    }
+
+    @Override
+    public void onLoggedOut() {
+        Log.d("MainActivity", "User logged out");
+    }
+
+    @Override
+    public void onLoginFailed(int i) {
+        Log.d("MainActivity", "Login failed");
+    }
+
+    @Override
+    public void onTemporaryError() {
+        Log.d("MainActivity", "Temporary error occurred");
+    }
+
+    @Override
+    public void onConnectionMessage(String message) {
+        Log.d("MainActivity", "Received connection message: " + message);
+    }
+
 }
